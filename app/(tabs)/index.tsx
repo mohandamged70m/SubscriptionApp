f0042360a87b7ed393ddea26042590813fe70204
mohandamged70m <mohandamged70m@icloud.com>
2026-04-07 01:08:25 +0200

    Revert "feat: project setup, nativewind, routing, tab navigation"

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,10 @@
 import "@/global.css"
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
-import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
-import { styled } from "nativewind";  
-
-const SafeAreaView = styled(RNSafeAreaView);
-
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 bg-background p-5">
+    <View className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -27,6 +22,6 @@ export default function App() {
       >
         Claude Max Subscription
       </Link>
-    </SafeAreaView>
+    </View>
   );
 }
--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,14 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { styled } from 'nativewind'
-import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context'
-const SafeAreaView = styled(RNSafeAreaView);
 
 const Insights = () => {
   return (
-    <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text>insights</Text>
-    </SafeAreaView>
+    <View>
+      <Text>insightI</Text>
+    </View>
   )
 }
 
